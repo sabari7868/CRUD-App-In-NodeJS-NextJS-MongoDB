d2fe2f86918b10bf25aec2d07bea63a9111e76bd
sabari7868 <sabarisurya540@gmail.com>
2024-11-12 23:28:08 +0530

    Update Connect.js

--- Backend/DB/Connect.js
+++ Backend/DB/Connect.js
@@ -1,2 +1,12 @@
-let Mongoose=require('mongoose')
-let XYX=Mongoose.connect('mongodb+srv://minecrudapp:minecrudapp123@cluster0.j2wknu4.mongodb.net/CRUD?retryWrites=true&w=majority')
+let mongoose = require('mongoose');
+
+// Replace <port> with the port number your MongoDB container is listening on (default is 27017)
+let MONGO_URI = 'mongodb://localhost:27018/CRUD';
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
