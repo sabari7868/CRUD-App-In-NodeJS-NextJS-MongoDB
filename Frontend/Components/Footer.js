fd0c25d73f2055d32765f9d7dcaf7730b465ce81
sabari7868 <sabarisurya540@gmail.com>
2024-11-28 14:56:40 +0530

    Update Footer.js

--- Frontend/Components/Footer.js
+++ Frontend/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-[#34495e] text-[#ecf0f1] p-3 absolute bottom-0 w-full text-center'>CRUD App - 2022</div>
+    <div className='bg-[#34495e] text-[#ecf0f1] p-3 absolute bottom-0 w-full text-center'>CRUD App - 2024</div>
   )
 }
 
