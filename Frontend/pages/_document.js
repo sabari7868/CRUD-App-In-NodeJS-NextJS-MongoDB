6f5f3e32e26c917b8ebe8a5fd6f923d2e1d8ab80
sabari7868 <sabarisurya540@gmail.com>
2024-12-02 17:04:11 +0530

    Update _document.js

--- Frontend/pages/_document.js
+++ Frontend/pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
         />
         <link rel="shortcut icon" href="Favicon.png" type="image/x-icon" />
       </Head>
-        <title>B2b product qwerty qwerew qwert qwertyuytrewwe qwertyuytrewq  qr</title>
+        <title>B2b</title>
       <body>
         <Main />
         <NextScript />
